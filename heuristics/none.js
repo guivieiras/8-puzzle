f6c672fa81edf0034c5e04e122c5f80fe80410a2
Guilherme Vieira <guilherme@feracode.com>
2019-07-13 19:50:02 -0300

    Removed unused files

--- heuristics/none.js
+++
@@ -1,9 +0,0 @@
-import { solvedPuzzle } from '../matrixes'
-import { isEqual } from '../helpers'
-
-export function heuristic({ matrix }) {
-    if (isEqual(matrix, solvedPuzzle.matrix)){
-        return 0;
-    }
-    return 1;
-}
