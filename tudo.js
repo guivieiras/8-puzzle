cd136983f2037677a2df435c7b46ff99bc581f76
Guilherme Vieira <guilherme@feracode.com>
2019-07-13 00:39:22 -0300

    Done

--- tudo.js
+++ tudo.js
@@ -1,290 +1,266 @@
-let heuristic = distanceToRightPlaceHeuristic // Distancia até o lugar certo
-// let heuristic = noneHeuristic // Custo uniforme
-// let heuristic = bothHeuristic // Ambas juntas
-// let heuristic = quantityAtWrongPlaceHeuristic // Quantidade no lugar errado
-
-//Puzzle.js
-
 class Puzzle {
 	constructor(matrix) {
-		this.matrix = matrix;
+		this.matrix = matrix
 	}
 
 	getNumberRowAndColumn(input) {
-		for (var [row, arrays] of this.matrix.entries()) {
-			for (var [column, number] of arrays.entries()) {
-				if (number == input)
-					return { row, column }
+		for (let [row, arrays] of this.matrix.entries()) {
+			for (let [column, number] of arrays.entries()) {
+				if (number == input) return { row, column }
 			}
 		}
-		return null;
+		return null
 	}
 
-	getAllPossibleMatrixes() {
-		var zeroPosition = this.getNumberRowAndColumn(0);
-		var matrixes = [
-			this.moveLeft(zeroPosition),
-			this.moveRight(zeroPosition),
-			this.moveDown(zeroPosition),
-			this.moveUp(zeroPosition)
-		]
+	openChildren() {
+		let zeroPosition = this.getNumberRowAndColumn(0)
+		let matrixes = [this.moveLeft(zeroPosition), this.moveRight(zeroPosition), this.moveDown(zeroPosition), this.moveUp(zeroPosition)]
 		return matrixes.filter(o => !!o)
 	}
 
 	moveLeft(position) {
 		if (position.column == 0) {
-			return null;
+			return null
 		}
-		return this.move(position, { row: position.row, column: position.column - 1 });
+		return this.move(position, { row: position.row, column: position.column - 1 })
 	}
 
 	moveRight(position) {
 		if (position.column == 2) {
-			return null;
+			return null
 		}
-		return this.move(position, { row: position.row, column: position.column + 1 });
+		return this.move(position, { row: position.row, column: position.column + 1 })
 	}
 	moveUp(position) {
 		if (position.row == 0) {
-			return null;
+			return null
 		}
-		return this.move(position, { row: position.row - 1, column: position.column });
+		return this.move(position, { row: position.row - 1, column: position.column })
 	}
 	moveDown(position) {
 		if (position.row == 2) {
-			return null;
+			return null
 		}
-		return this.move(position, { row: position.row + 1, column: position.column });
+		return this.move(position, { row: position.row + 1, column: position.column })
 	}
 
 	cloneMatrix() {
-		return JSON.parse(JSON.stringify(this.matrix));
+		return JSON.parse(JSON.stringify(this.matrix))
 	}
 
 	move(oldPosition, newPosition) {
-		let matrix = this.cloneMatrix();
-		var number = matrix[oldPosition.row][oldPosition.column];
-		var temp = matrix[newPosition.row][newPosition.column];
-		matrix[newPosition.row][newPosition.column] = number;
-		matrix[oldPosition.row][oldPosition.column] = temp;
-		return new Puzzle(matrix);
+		let matrix = this.cloneMatrix()
+		let number = matrix[oldPosition.row][oldPosition.column]
+		let temp = matrix[newPosition.row][newPosition.column]
+		matrix[newPosition.row][newPosition.column] = number
+		matrix[oldPosition.row][oldPosition.column] = temp
+		return new Puzzle(matrix)
 	}
 	getMatrix() {
-		return this.matrix;
+		return this.matrix
 	}
 	getPosition(input) {
-		for (var [x, arrays] of this.matrix.entries()) {
-			for (var [y, number] of arrays.entries()) {
-				if (number == input)
-					return { x, y }
+		for (let [x, arrays] of this.matrix.entries()) {
+			for (let [y, number] of arrays.entries()) {
+				if (number == input) return { x, y }
 			}
 		}
-		return null;
+		return null
 	}
 }
 
 // Helpers.js
 
-function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)) }
+function isEqual(matrix, otherMatrix) {
+	if (matrix.length != otherMatrix.length) {
+		return false
+	}
 
-function isEqual(value, other) {
-	// Get the value type
-	var type = Object.prototype.toString.call(value);
-
-	// If the two objects are not the same type, return false
-	if (type !== Object.prototype.toString.call(other)) return false;
-
-	// If items are not an object or array, return false
-	if (['[object Array]', '[object Object]'].indexOf(type) < 0) return false;
-
-	// Compare the length of the length of the two items
-	var valueLen = type === '[object Array]' ? value.length : Object.keys(value).length;
-	var otherLen = type === '[object Array]' ? other.length : Object.keys(other).length;
-	if (valueLen !== otherLen) return false;
-
-	// Compare two items
-	var compare = function (item1, item2) {
-
-		// Get the object type
-		var itemType = Object.prototype.toString.call(item1);
-
-		// If an object or array, compare recursively
-		if (['[object Array]', '[object Object]'].indexOf(itemType) >= 0) {
-			if (!isEqual(item1, item2)) return false;
+	for (let i = 0; i < matrix.length; i++) {
+		if (matrix[i].length != otherMatrix[i].length) {
+			return false
 		}
 
-		// Otherwise, do a simple comparison
-		else {
-
-			// If the two items are not the same type, return false
-			if (itemType !== Object.prototype.toString.call(item2)) return false;
-
-			// Else if it's a function, convert to a string and compare
-			// Otherwise, just compare
-			if (itemType === '[object Function]') {
-				if (item1.toString() !== item2.toString()) return false;
-			} else {
-				if (item1 !== item2) return false;
-			}
-
-		}
-	};
-
-	// Compare properties
-	if (type === '[object Array]') {
-		for (var i = 0; i < valueLen; i++) {
-			if (compare(value[i], other[i]) === false) return false;
-		}
-	} else {
-		for (var key in value) {
-			if (value.hasOwnProperty(key)) {
-				if (compare(value[key], other[key]) === false) return false;
+		for (let j = 0; j < matrix[i].length; j++) {
+			if (matrix[i][j] != otherMatrix[i][j]) {
+				return false
 			}
 		}
 	}
 
-	// If nothing failed, return true
-	return true;
+	return true
+}
 
-};
+// Matrixes
+const solvedPuzzle = new Puzzle([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
 
-// Matrixes.js
+let wrongPuzzles = [new Puzzle([[8, 3, 6], [1, 0, 2], [4, 7, 5]]), new Puzzle([[0, 8, 7], [6, 5, 4], [3, 2, 1]]), new Puzzle([[0, 1, 2], [5, 6, 3], [4, 7, 8]])]
 
-const solvedPuzzle = new Puzzle(
-	[
-		[1, 2, 3],
-		[4, 5, 6],
-		[7, 8, 0]
-	]);
+const toSolvePuzzle = wrongPuzzles[0]
 
-
-var wrongPuzzles = [new Puzzle(
-	[
-		[8, 3, 6],
-		[1, 0, 2],
-		[4, 7, 5]
-	]), new Puzzle([
-		[0, 8, 7],
-		[6, 5, 4],
-		[3, 2, 1]
-	]), new Puzzle([
-		[0, 1, 2],
-		[5, 6, 3],
-		[4, 7, 8]
-	])]
-
-const toSolvePuzzle = wrongPuzzles[0];
-
-
-// Run.js
-
-var openNodes = [{ puzzle: toSolvePuzzle, heuristic: heuristic(toSolvePuzzle), parent: null }]
-var maiorFronteira = 1;
-var closedNodes = []
-function teste() {
-	while (true) {
-		let lower = openNodes.reduce((prev, curr) => prev.final < curr.final ? prev : curr)
-		closedNodes.push(lower);
-		openNodes = openNodes.filter(i => i != lower);
-
-		console.log(lower.final);
-
-		if (lower.heuristic == 0) {
-			console.log('fim')
-			console.log({ closed: closedNodes.length, open: openNodes.length })
-			console.log(lower)
-			console.log(maiorFronteira)
-			return;
+// Método principal
+function run(heuristic) {
+	// Fronteira ordenada
+	let borderland = [
+		{
+			puzzle: toSolvePuzzle,
+			heuristic: heuristic(toSolvePuzzle),
+			totalCost: getTotalCost(toSolvePuzzle),
+			parent: null
 		}
-		console.log(lower.heuristic)
+	]
+	let maiorFronteira = 1
+	let closedNodes = []
+	while (true) {
+		// Retorna o nodo de menor custo + heuristica, ou seja, o primeiro, e o remove da fronteira
+		let lower = borderland.shift()
 
-		let puzzles = lower.puzzle.getAllPossibleMatrixes();
+		// Adiciona nodo na lista de nodos fechados
+		closedNodes.push(lower)
+
+		// Se nodo for uma folha
+		if (lower.heuristic == 0) {
+			print('fim')
+			print({ closed: closedNodes.length, open: borderland.length })
+			print({ lower })
+			print({ maiorFronteira })
+			return
+		}
+
+		// Abre os filhos deste nodo
+		let puzzles = lower.puzzle.openChildren()
 		for (let m of puzzles) {
+			// É verdadeiro caso nodo filho já exista na lista de nodos fechados
+			let existsOnClosed = closedNodes.filter(o => isEqual(o.puzzle.matrix, m.matrix)).length != 0
 
+			// É verdadeiro caso nodo filho já exista na fronteira
+			let existsOnBorderland = borderland.filter(o => isEqual(o.puzzle.matrix, m.matrix)).length != 0
 
-			if (closedNodes.filter(o => isEqual(o.puzzle.matrix, m.matrix)).length == 0) {
-				let obj = { puzzle: m, heuristic: heuristic(m), parent: lower };
-				obj.pathSize = getPathSize(obj);
-				obj.final = obj.pathSize + obj.heuristic;
-				openNodes.push(obj)
+			//Se não existir nem na fronteira nem em nodos fechados, adiciona à fronteira
+			if (!existsOnBorderland && !existsOnClosed) {
+				let node = { puzzle: m, heuristic: heuristic(m), parent: lower }
+				node.totalCost = getTotalCost(node)
+
+				// Algoritmo para colocar o nodo de forma ordenada na fronteira
+				for (let i = 0; i < borderland.length; i++) {
+					if (node.heuristic + node.totalCost <= borderland[i].heuristic + borderland[i].totalCost) {
+						borderland.splice(i, 0, node)
+						break
+					}
+					if (i == borderland.length - 1) {
+						borderland.push(node)
+						break
+					}
+				}
+				if (borderland.length == 0) {
+					borderland.push(node)
+				}
 			}
 		}
-		maiorFronteira = Math.max(openNodes.length, maiorFronteira);
+		// Guarda valor da maior fronteira existente
+		maiorFronteira = Math.max(borderland.length, maiorFronteira)
 	}
 }
-function getPathSize(obj) {
-	let tamanhoCaminho = 0;
+
+// Retorna o custo total, basicamente contando os nodos pai até chegar no topo da árvore
+function getTotalCost(obj) {
+	let tamanhoCaminho = 0
 	let parent = obj.parent
 	while (parent) {
-		tamanhoCaminho++;
-		parent = parent.parent;
+		tamanhoCaminho++
+		parent = parent.parent
 	}
-	return tamanhoCaminho;
-
-}
-
-// Both
-
-function bothHeuristic(puzzle) {
-	var sum = 0;
-
-	for (var [i, arrays] of puzzle.matrix.entries()) {
-		for (var [j, number] of arrays.entries()) {
-			var { distance, number } = distanceToRightPlace(number, i, j)
-			sum += distance;
-			sum += isAtRightPlace(number, i, j) ? 0 : 1;
-		}
-	}
-	return sum;
+	return tamanhoCaminho
 }
 
 // Distance to right place
-
 function distanceToRightPlace(number, i, j) {
-	var { x, y } = solvedPuzzle.getPosition(number);
-	var distance = Math.abs(i - x) + Math.abs(j - y);
-	if (!number)
-		distance = 0;
-	return distance;
+	let { x, y } = solvedPuzzle.getPosition(number)
+	let distance = Math.abs(i - x) + Math.abs(j - y)
+	if (!number) distance = 0
+	return distance
 }
 
 function distanceToRightPlaceHeuristic(puzzle) {
-	var sum = 0;
+	let sum = 0
 
-	for (var [i, arrays] of puzzle.matrix.entries()) {
-		for (var [j, number] of arrays.entries()) {
-			sum += distanceToRightPlace(number, i, j);
+	for (let [i, arrays] of puzzle.matrix.entries()) {
+		for (let [j, number] of arrays.entries()) {
+			sum += distanceToRightPlace(number, i, j)
 		}
 	}
-	return sum;
+	return sum
 }
 
 // Quantity at wrong place
-
 function isAtRightPlace(number, i, j) {
-	var { x, y } = solvedPuzzle.getPosition(number);
-	return x == i && j == y;
+	let { x, y } = solvedPuzzle.getPosition(number)
+	return x == i && j == y
 }
 
 function quantityAtWrongPlaceHeuristic(puzzle) {
-	var sum = 0;
+	let sum = 0
 
-	for (var [i, arrays] of puzzle.matrix.entries()) {
-		for (var [j, number] of arrays.entries()) {
-			sum += isAtRightPlace(number, i, j) ? 0 : 1;
+	for (let [i, arrays] of puzzle.matrix.entries()) {
+		for (let [j, number] of arrays.entries()) {
+			sum += isAtRightPlace(number, i, j) ? 0 : 1
 		}
 	}
-	return sum;
+	return sum
 }
 
-// None
+// None (Custo uniforme)
 
 function noneHeuristic({ matrix }) {
 	if (isEqual(matrix, solvedPuzzle.matrix)) {
-		return 0;
+		return 0
 	}
-	return 1;
+	return 1
 }
 
+let print
+function start() {
+	try {
+		// Html
+		let code = document.getElementById('code')
 
-teste()
+		print = text => {
+			if (!document) {
+				console.log(text)
+				return
+			}
+			if (typeof text == 'object') {
+				text = JSON.stringify(text, null, 2)
+			}
+			code.innerText += text + '\n'
+		}
+
+		document.getElementById('h1').addEventListener('click', () => run(distanceToRightPlaceHeuristic))
+		document.getElementById('h2').addEventListener('click', () => run(quantityAtWrongPlaceHeuristic))
+		document.getElementById('none').addEventListener('click', () => run(noneHeuristic))
+	} catch (error) {
+		// Node
+
+		print = text => console.log(text)
+
+		const readline = require('readline')
+
+		const rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout
+		})
+
+		rl.question('Run which heuristic? \n(1) Distancia até o lugar certo \n(2) Quantidade no lugar errado\n(3) Custo uniforme\n', answer => {
+			rl.close()
+			switch (answer) {
+				case '1':
+					return run(distanceToRightPlaceHeuristic)
+				case '2':
+					return run(quantityAtWrongPlaceHeuristic)
+				case '3':
+					return run(noneHeuristic)
+			}
+		})
+	}
+}
+start()
